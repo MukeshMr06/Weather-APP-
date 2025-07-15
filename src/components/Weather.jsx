9d1780617b862e4883237499c389dc966ba131ec
MukeshMr06 <mukeshmr0602@gmail.com>
2025-07-15 11:20:35 -0500

    changes made

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -18,16 +18,16 @@ const Weather = () => {
         setSearch("")
     }
 
-    // useEffect(() => {
-    //     getweather()
-    // }, [search])
+    useEffect(() => {
+        getweather()
+    },[])
 
     return (
-        <div className='weather w-[700px] flex flex-col gap-10'>
+        <div className='weather w-[450px] md:w-[700px] flex flex-col gap-10'>
             <div className="searchbar gap-3 w-full">
                 <div className='flex justify-center gap-3'>
-                    <input type="text" placeholder='Enter a city' value={search} onChange={(e) => setSearch(e.target.value)} className='rounded-2xl outline-none bg-white w-[400px] p-2' />
-                    <button onClick={getweather}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 mt-4 bg-white rounded-full cursor-pointer">
+                    <input type="search" placeholder='Enter a city' value={search} onChange={(e) => setSearch(e.target.value)} className='rounded-2xl outline-none bg-white w-[400px] h-10 p-2' />
+                    <button onClick={getweather}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-8 mt-4 bg-white rounded-full cursor-pointer">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                     </svg>
                     </button>
@@ -41,18 +41,18 @@ const Weather = () => {
                 <p className='font-bold text-4xl text-white'>{city?.name}</p>
             </div>
 
-            <div className='flex justify-between w-full '>
+            <div className='flex justify-between w-full'>
                 <div className='flex items-center gap-3.5'>
-                    <img src="./humdity.png" alt="" className='w-20' />
-                    <div className='text-2xl'>
+                    <img src="./humdity.png" alt="" className='w-16 md:w-20' />
+                    <div className='md:text-2xl'>
                         <p>{city?.main?.humidity}%</p>
                         <span>Humidity</span>
                     </div>
                 </div>
 
                 <div className='flex items-center gap-3.5'>
-                    <img src="wind.png" alt="" className='w-20' />
-                    <div className='text-2xl'>
+                    <img src="wind.png" alt="" className='w-16 md:w-20' />
+                    <div className='md:text-2xl'>
                         <p>{city?.wind?.speed}km/hr</p>
                         <span>Speed</span>
                     </div>
